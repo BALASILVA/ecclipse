ab5c78590c581aeb097073c12199072ed142cab4
BALASILVA <balasilvabe@gmail.com>
2017-06-21 09:26:37 +0530

    Add files via upload

---
+++ SumNat.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class SumNat {
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the uypper limit");
+		int limit=sc.nextInt();
+	sc.close();
+	int sum=0;
+	for(int i=1;i<=limit;i++){
+		sum=sum+i;
+	}
+	System.out.println("The sum of number N natural number is:"+sum);
+	}
+	}
+
