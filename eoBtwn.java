6f8ba5ef48369b713d39052b49d5ed832fdb982e
BALASILVA <balasilvabe@gmail.com>
2017-06-28 09:29:27 +0530

    Add files via upload

---
+++ eoBtwn.java
@@ -0,0 +1,16 @@
+package fibonasis;
+import java.util.Scanner;
+public class eoBtwn {
+		public static void main(String[]args){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number");
+	int num1=sc.nextInt();
+	int num2=sc.nextInt();
+	sc.close();
+	for(int i=num1;i<=num2;i++){
+	if(i%2==0){
+		System.out.println(i);
+      }
+	}
+	}
+}
