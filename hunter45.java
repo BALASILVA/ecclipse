ccfeeb700b24d78ab2101b80635e61665c96d98c
BALASILVA <balasilvabe@gmail.com>
2017-07-17 03:22:12 -0700

    Add files via upload

---
+++ hunter45.java
@@ -0,0 +1,14 @@
+package Hunter;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class hunter45 {
+public static void main(String[]args){
+	String []str={"fd","Fd","ert","f","gfg"};
+	for(int i=0;i<str.length;i++){
+		if(str[i].length()==1){
+			System.out.println(str[i]);
+		}
+	}
+}
+}
